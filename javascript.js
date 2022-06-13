906045e92312ff2b02a6cda4009591c3ba29c65f
mahmad <ahmadqwerty123@gmail.com>
2022-06-13 14:13:41 +0500

    Make a calculator working for first 2 operands

--- javascript.js
+++ javascript.js
@@ -1,41 +1,57 @@
-const add = function(...a) {
-	let x = 0;
-  a.forEach(e => x += e);
-  return x;
+const add = function(a,b) {
+  return (1 * a) + b;
 };
-
 const subtract = function(a,b) {
-	return a - b;
+	return (1 * a) - b;
 };
+const multiply = function(a,b) {
+  return (1 * a) * b;
+};
+const divide = function(a,b) {
+  return (1 * a) / b;
+};
+const operate = function(o,a,b) {
+  if (o == `+`) return add(a,b);
+  if (o == `-`) return subtract(a,b);
+  if (o == `*`) return multiply(a,b);
+  if (o == `/`) return divide(a,b);
+}
 
-const sum = function(a) {
-	let x = 0;
-  a.forEach(e => x += e);
-  return x;
-};
+const numDown = function(e) {
+  console.log(e);
+  currentOperand += e.target.textContent; 
+  displayStr += e.target.textContent;
+  const display = document.querySelector(`#display`);
+  display.textContent = displayStr;
+}
 
-const multiply = function(a) {
-  let x = 1;
-  a.forEach(e => x *= e);
-  return x;
-};
+const opDown = function(e) {
+  console.log(e);
+  if (firstOperand == ``) {firstOperand = currentOperand;}
+  else {secondOperand = currentOperand;}
+  currentOperand = ``;
+  if (currentOperator == ``) {currentOperator = e.target.textContent;}
+  else {
+    const currentAnswer = operate(currentOperator,firstOperand,secondOperand);
+    firstOperand = currentAnswer;
+    displayStr = currentAnswer;
+  }
+  displayStr += e.target.textContent;
+  const display = document.querySelector(`#display`);
+  display.textContent = displayStr;
+}
 
-const power = function(a,b) {
-	return a ** b;
-};
+let displayStr = ``;
+let currentOperand = ``;
+let currentOperator = ``;
+let firstOperand = ``;
+let secondOperand = ``;
 
-const factorial = function(a) {
-	let x = 1;
-  for (let i = 1; i <= a; i++) {x *= i};
-  return x;
-};
-
-// Do not edit below this line
-module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+const numbers = document.querySelectorAll(`.number`);
+numbers.forEach(num => {
+  num.addEventListener(`click`,numDown); 
+});
+const operators = document.querySelectorAll(`.operator`);
+operators.forEach(op => {
+  op.addEventListener(`click`,opDown);  
+});
